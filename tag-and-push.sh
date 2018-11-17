24317e1ad0b0308b58a1407510c0895ffa6a6684
Ron Kurr <rkurr@overstock.com>
2018-11-17 21:42:36 +0000

    Updated to JDK 11.

--- tag-and-push.sh
+++ tag-and-push.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # use the time as a tag
-UNIXTIME=$(date +%s)
+UNIXTIME=$(date +%F)
 
 # docker tag SOURCE_IMAGE[:TAG] TARGET_IMAGE[:TAG]
-docker tag docker-azul-jdk-10-build_azul-jdk-10:latest kurron/docker-azul-jdk-10-build:latest
-docker tag docker-azul-jdk-10-build_azul-jdk-10:latest kurron/docker-azul-jdk-10-build:${UNIXTIME}
+docker tag docker-azul-jdk-11-build_azul-jdk-11:latest kurron/docker-azul-jdk-11-build:latest
+docker tag docker-azul-jdk-11-build_azul-jdk-11:latest kurron/docker-azul-jdk-11-build:${UNIXTIME}
 docker images
 
 # Usage:  docker push [OPTIONS] NAME[:TAG]
-docker push kurron/docker-azul-jdk-10-build:latest
-docker push kurron/docker-azul-jdk-10-build:${UNIXTIME}
+docker push kurron/docker-azul-jdk-11-build:latest
+docker push kurron/docker-azul-jdk-11-build:${UNIXTIME}
